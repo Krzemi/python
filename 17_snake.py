3a74ff4745b32761038ec2428c66d12d1c8a174c
Krzemi <98230333+Krzemi@users.noreply.github.com>
2022-03-01 19:27:34 +0100

    final

--- 17_snake.py
+++ 17_snake.py
@@ -4,8 +4,8 @@ import random
 def random_location():
     global SCREEN_SIZE, FOOD_LOCATION
     return (
-        random.randint(0, SCREEN_SIZE[0]/10 * 10),
-        random.randint(0, SCREEN_SIZE[1]/10 * 10)
+        random.randint(0, SCREEN_SIZE[0]/10 - 1) * 10,
+        random.randint(0, SCREEN_SIZE[1]/10 - 1) * 10
     )
 
 # RGB (Red, Green, Blue)
@@ -19,6 +19,10 @@ pygame.init()
 SCREEN_SIZE = (800, 600)
 SCREEN = pygame.display.set_mode(SCREEN_SIZE)
 pygame.display.set_caption("Snake")
+pygame.font.init()
+
+czcionka = pygame.font.SysFont('Chiller', 30)
+czcionka60 = pygame.font.SysFont('Freestyle Script', 30)
 
 clock = pygame.time.Clock()
 
@@ -33,6 +37,7 @@ vx = 10
 vy = 0
 
 FOOD_LOCATION = None
+game_over = False
 
 while True:
     for event in pygame.event.get():
@@ -41,50 +46,76 @@ while True:
             quit()
 
         if event.type == pygame.KEYDOWN:
-           if event.key == pygame.K_LEFT:
-               vx = -10
-               vy = 0
-           elif event.key == pygame.K_RIGHT:
-               vx = 10
-               vy = 0
-           elif event.key == pygame.K_UP:
-               vx = 0
-               vy = -10
-           elif event.key == pygame.K_DOWN:
+            if event.key == pygame.K_LEFT:
+                vx = -10
+                vy = 0
+            elif event.key == pygame.K_RIGHT:
+                vx = 10
+                vy = 0
+            elif event.key == pygame.K_UP:
+                vx = 0
+                vy = -10
+            elif event.key == pygame.K_DOWN:
                vx = 0
                vy = 10
+            elif event.key == pygame.K_RETURN:
+                game_over = False
+                segments = [segments[0]]
 
-    snake_x += vx
-    snake_y += vy
+    if game_over:
+        SCREEN.fill(BLACK)
+        tekst_koniec = czcionka60.render(f'Twój wynik: {len(segments) - 1}', False, WHITE)
+        tekst_reset = czcionka60.render('Wciśnij ENTER, aby zagrać ponownie', False, WHITE)
 
-    WIDTH, HEIGHT = SCREEN_SIZE
-    if snake_x >=WIDTH:
-        snake_x = 0
-    elif snake_x < 0:
-        snake_x = WIDTH - 10
-    if snake_y >= HEIGHT:
-        snake_y = 0
-    elif snake_y < 0:
-        snake_y = HEIGHT - 10
+        SCREEN.blit(tekst_koniec, (10, 10))
+        SCREEN.blit(tekst_reset, (10, 80))
+    else:
 
-    segments.insert(0, (snake_x, snake_y))
+        snake_x += vx
+        snake_y += vy
+
+        WIDTH, HEIGHT = SCREEN_SIZE
+        if snake_x >=WIDTH:
+            snake_x = 0
+        elif snake_x < 0:
+            snake_x = WIDTH - 10
+        if snake_y >= HEIGHT:
+            snake_y = 0
+        elif snake_y < 0:
+            snake_y = HEIGHT - 10
+
+        if (snake_x, snake_y) in segments:
+            game_over = True
+
+        segments.insert(0, (snake_x, snake_y))      # dodaj na poczatku
         
-    
-    if (FOOD_LOCATION
-        and snake_x == FOOD_LOCATION[2]
-        and snake_y == FOOD_LOCATION[1]):
-        FOOD_LOCATION = None
+        if (FOOD_LOCATION
+            and snake_x == FOOD_LOCATION[0]
+            and snake_y == FOOD_LOCATION[1]):
+            FOOD_LOCATION = None
+        else:
+            segments.pop()      # usun na koncu listy
 
-    if FOOD_LOCATION is None:
-        FOOD_LOCATION = random_location()
+        if FOOD_LOCATION is None:
+            FOOD_LOCATION = random_location()
 
 
-    SCREEN.fill(BLACK)
-    for x, y in segments:
+        SCREEN.fill(BLACK)
+        for x, y in segments:
+            pygame.draw.rect(
+                SCREEN ,WHITE, [x, y, 10, 10])
         pygame.draw.rect(
-            SCREEN ,WHITE, [snake_x, snake_y, 10, 10])
-    pygame.draw.rect(
-        SCREEN, RED, [FOOD_LOCATION[0], FOOD_LOCATION[1], 10, 10])
+            SCREEN, RED, [FOOD_LOCATION[0], FOOD_LOCATION[1], 10, 10])
+
+        tekst_punkty = czcionka.render(f'Punkty: {len(segments) - 1}', False, WHITE)
+        SCREEN.blit(tekst_punkty, (10, 10))
 
     pygame.display.update()
     clock.tick(30)
+
+
+
+
+    ##########################################################################################################
+                # NA POZNIEJ DOPISAC KOD, KTORY ROBI DUZE PUNKTY!!!
+    ##########################################################################################################
